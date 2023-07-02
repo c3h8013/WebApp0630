93cf45af5b66121a851d1c87e550bd810d889f42
Bob <c3h8013@gmail.com>
2023-07-02 15:18:33 +0800

    調整連結去呼叫API的動作

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,30 +8,11 @@ namespace WebApp0630.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly HttpClient httpClient;
-        public HomeController()
+        public ActionResult Index()
         {
-            httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("https://localhost:44310"); // 設定API的基礎位址
-        }
+            ViewBag.Title = "Home Page";
 
-        public async Task<ActionResult> Index()
-        {
-            //ViewBag.Title = "Home Page";
-
-            //return View();
-
-            HttpResponseMessage response = await httpClient.GetAsync("/api/Weather/GetData"); // 發送GET請求到API的特定端點
-            if (response.IsSuccessStatusCode)
-            {
-                string responseData = await response.Content.ReadAsStringAsync();
-                return View("Index", responseData); // 將資料傳遞給檢視
-            }
-            else
-            {
-                // 處理錯誤情況
-                return View("Error");
-            }
+            return View();
         }
     }
 }
