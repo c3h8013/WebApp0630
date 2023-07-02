354c584bdede7a941bd964d86edb7c5d8be40f3f
Bob <c3h8013@gmail.com>
2023-07-02 13:41:37 +0800

    加入controller以及model

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,84 @@
+﻿//using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApp0630.Models;
+
+
+
+
+namespace WebApp0630.Controllers
+{
+    public class WeatherController : ApiController
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public WeatherController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+
+        // GET: Weather
+        public async Task<IEnumerable<Weather36HourViewModel>> GetData()
+        {
+            // 先取得最新的資料
+            string url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-0F81C956-3210-42C0-9F8A-5033C96EBA53";
+
+            string result = string.Empty;
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    // Handle error response
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+            }
+
+            // 資料轉物件
+            RootObject root = JsonConvert.DeserializeObject<RootObject>(result);
+
+            // 物件處理 資料攤平
+            var datetimeNow = DateTime.Now;
+            var flattenedData = root.Records.Location
+            .SelectMany(loc => loc.WeatherElement
+                .SelectMany(we => we.Time
+                    .Select(t => new Weather36HourViewModel()
+                    {
+                        DatasetDescription = root.Records.DatasetDescription,
+                        LocationName = loc.LocationName,
+                        ElementName = we.ElementName,
+                        StartTime = t.StartTime,
+                        EndTime = t.EndTime,
+                        ParameterName = t.Parameter.ParameterName,
+                        ParameterValue = t.Parameter.ParameterValue,
+                        ParameterUnit = t.Parameter.ParameterUnit
+                    })
+                )
+            );
+
+            // 資料儲存
+
+            // 回傳資料
+            return flattenedData;
+        }
+    }
+}
---
+++ Models/Weather36HourViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp0630.Models
+{
+    public class Weather36HourViewModel
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 名稱
+        /// </summary>
+        public string DatasetDescription { get; set; }
+        /// <summary>
+        /// 臺灣各縣市
+        /// </summary>
+        public string LocationName { get; set; }
+        /// <summary>
+        /// 天氣因子
+        /// </summary>
+        public string ElementName { get; set; }
+        /// <summary>
+        /// 時間因子
+        /// </summary>
+        public DateTime StartTime { get; set; }
+        /// <summary>
+        /// 時間因子
+        /// </summary>
+        public DateTime EndTime { get; set; }
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
+        public string ParameterUnit { get; set; }
+        /// <summary>
+        /// 新增時間
+        /// </summary>
+        public DateTime AddTime { get; set; }
+    }
+
+    public class RootObject
+    {
+        public bool Success { get; set; }
+        public ResultObject Result { get; set; }
+        public RecordsObject Records { get; set; }
+    }
+
+    public class ResultObject
+    {
+        public string ResourceId { get; set; }
+        public List<FieldObject> Fields { get; set; }
+    }
+
+    public class FieldObject
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class RecordsObject
+    {
+        public string DatasetDescription { get; set; }
+        public List<LocationObject> Location { get; set; }
+    }
+
+    public class LocationObject
+    {
+        public string LocationName { get; set; }
+        public List<WeatherElementObject> WeatherElement { get; set; }
+    }
+
+    public class WeatherElementObject
+    {
+        public string ElementName { get; set; }
+        public List<TimeObject> Time { get; set; }
+    }
+
+    public class TimeObject
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public ParameterObject Parameter { get; set; }
+    }
+
+    public class ParameterObject
+    {
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
+        public string ParameterUnit { get; set; }
+    }
+}
