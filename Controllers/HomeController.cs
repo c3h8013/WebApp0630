c6576c10d4b3c7ac61dfc763131cb76886375b18
Bob <c3h8013@gmail.com>
2023-07-02 14:59:56 +0800

    調整進入首頁就呼叫API的動作，調整HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,30 @@ namespace WebApp0630.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly HttpClient httpClient;
+        public HomeController()
         {
-            ViewBag.Title = "Home Page";
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44310"); // 設定API的基礎位址
+        }
 
-            return View();
+        public async Task<ActionResult> Index()
+        {
+            //ViewBag.Title = "Home Page";
+
+            //return View();
+
+            HttpResponseMessage response = await httpClient.GetAsync("/api/Weather/GetData"); // 發送GET請求到API的特定端點
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                return View("Index", responseData); // 將資料傳遞給檢視
+            }
+            else
+            {
+                // 處理錯誤情況
+                return View("Error");
+            }
         }
     }
 }
