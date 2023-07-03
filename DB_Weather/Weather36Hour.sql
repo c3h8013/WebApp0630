bddf6666ee5de027d8f97dba0f7dc0bd284d08bf
Bob <c3h8013@gmail.com>
2023-07-03 16:06:52 +0800

    重新上傳DB專案

---
+++ DB_Weather/Weather36Hour.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Weather36Hour](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DatasetDescription] [nvarchar](50) NOT NULL,
+	[LocationName] [nvarchar](50) NOT NULL,
+	[ElementName] [nvarchar](50) NOT NULL,
+	[StartTime] [datetime] NOT NULL,
+	[EndTime] [datetime] NOT NULL,
+	[ParameterName] [nvarchar](50) NULL,
+	[ParameterValue] [nvarchar](50) NULL,
+	[ParameterUnit] [nvarchar](50) NULL,
+	[AddTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_Weather36Hour] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
